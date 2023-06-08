af41edd67e03703a4a67cc0e59df4695a9e8c208
khakimdon <almamedovkh@mail.ru>
2023-06-08 20:17:36 +0500

    six

--- src/components/FourthScreen.vue
+++ src/components/FourthScreen.vue
@@ -1,60 +1,152 @@
 <template>
-  <h1 class="mt-[200px] 666:mt-[150px] 666:mb-[0px] 1200:text-[40px] heading 666:text-[20px] text-[60px] mb-[40px] pop font-[poppins] text-center font-[500]">Why us ?</h1>
-  <div class="min-h-[700px] 1200:max-w-[100%] 1200:flex-col 1500:w-[1200px] items-center justify-between flex flex-wrap w-[1500px] mx-auto w-[100%]">
-  <div class="shadow-xl 666:w-[95%] 666:mt-[10px] gy shadow-[#14140010] flex p-[40px] 666:p-[20px] 666:pb-[40px] mt-[30px] w-[80%] rounded-[40px] w-[45%]">
-      <div class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] yg mr-[40px] rounded-[20px] w-[75px] bg-[purple]">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-</svg>
-
+  <h1
+    class="mt-[200px] 666:mt-[150px] 666:mb-[0px] 1200:text-[40px] heading 666:text-[20px] text-[60px] mb-[40px] pop font-[poppins] text-center font-[500]"
+  >
+    Why us ?
+  </h1>
+  <div
+    class="min-h-[700px] 1200:max-w-[100%] 1200:flex-col 1500:w-[1200px] items-center justify-between flex flex-wrap w-[1500px] mx-auto w-[100%]"
+  >
+    <div
+      class="shadow-xl x 666:w-[95%] 666:mt-[10px] gy shadow-[#14140010] flex p-[40px] 666:p-[20px] 666:pb-[40px] mt-[30px] w-[80%] rounded-[40px] w-[45%]"
+    >
+      <div
+        class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] yg mr-[40px] rounded-[20px] w-[75px] bg-[purple]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
+          />
+        </svg>
       </div>
       <div class="flex-1">
-        <span class="text-[40px] pop font-[500] 1200:text-[28px]">Savety camping</span>
-        <p class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia molestias recusandae molestiae maiores deserunt numquam sunt commodi eveniet ipsa unde!
+        <span class="text-[40px] pop font-[500] 1200:text-[28px]"
+          >Savety camping</span
+        >
+        <p
+          class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia
+          molestias recusandae molestiae maiores deserunt numquam sunt commodi
+          eveniet ipsa unde!
         </p>
       </div>
     </div>
-    <div class="shadow-xl 666:w-[95%] gy shadow-[#14140010] 666:p-[20px] 666:pb-[40px]  flex p-[40px] mt-[30px] w-[80%] rounded-[40px] w-[45%]">
-      <div class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] mr-[40px] yg rounded-[20px] w-[75px] bg-[purple]">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
-</svg>
 
+    <div
+      class="shadow-xl x 666:w-[95%] gy shadow-[#14140010] 666:p-[20px] 666:pb-[40px] flex p-[40px] mt-[30px] w-[80%] rounded-[40px] w-[45%]"
+    >
+      <div
+        class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] mr-[40px] yg rounded-[20px] w-[75px] bg-[purple]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25"
+          />
+        </svg>
       </div>
       <div class="flex-1">
-        <span class="text-[40px] pop font-[500] 1200:text-[28px]">Savety camping</span>
-        <p class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia molestias recusandae molestiae maiores deserunt numquam sunt commodi eveniet ipsa unde!
+        <span class="text-[40px] pop font-[500] 1200:text-[28px]"
+          >Savety camping</span
+        >
+        <p
+          class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia
+          molestias recusandae molestiae maiores deserunt numquam sunt commodi
+          eveniet ipsa unde!
         </p>
       </div>
     </div>
-    <div class="shadow-xl 666:w-[95%] gy 666:p-[20px] 666:pb-[40px] shadow-[#14140010]  flex p-[40px] : mt-[30px] w-[80%] rounded-[40px] w-[45%]">
-      <div class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] mr-[40px] rounded-[20px] yg w-[75px] bg-[purple]">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-  <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
-</svg>
-
+    
+    <div
+      class="shadow-xl x 666:w-[95%] gy 666:p-[20px] 666:pb-[40px] shadow-[#14140010] flex p-[40px] : mt-[30px] w-[80%] rounded-[40px] w-[45%]"
+    >
+      <div
+        class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] mr-[40px] rounded-[20px] yg w-[75px] bg-[purple]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z"
+          />
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"
+          />
+        </svg>
       </div>
       <div class="flex-1">
-        <span class="text-[40px] pop font-[500] 1200:text-[28px]">Savety camping</span>
-        <p class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia molestias recusandae molestiae maiores deserunt numquam sunt commodi eveniet ipsa unde!
+        <span class="text-[40px] pop font-[500] 1200:text-[28px]"
+          >Savety camping</span
+        >
+        <p
+          class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia
+          molestias recusandae molestiae maiores deserunt numquam sunt commodi
+          eveniet ipsa unde!
         </p>
       </div>
     </div>
-    <div class="shadow-xl 666:w-[95%] gy 666:p-[20px] 666:pb-[40px] shadow-[#14140010]  flex p-[40px] : mt-[30px] w-[80%] rounded-[40px] w-[45%]">
-      <div class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] yg mr-[40px] rounded-[20px] w-[75px] bg-[purple]">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
-</svg>
-
+    <div
+      class="shadow-xl x 666:w-[95%] gy 666:p-[20px] 666:pb-[40px] shadow-[#14140010] flex p-[40px] : mt-[30px] w-[80%] rounded-[40px] w-[45%]"
+    >
+      <div
+        class="h-[70px] flex items-center justify-center 1200:w-[65px] 1200:h-[60px] 666:mr-[20px] yg mr-[40px] rounded-[20px] w-[75px] bg-[purple]"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-[35px] h-[35px] 1200:w-[30px] h-[30px ] text-white"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+          />
+        </svg>
       </div>
       <div class="flex-1">
-        <span class="text-[40px] pop font-[500] 1200:text-[28px]">Savety camping</span>
-        <p class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia molestias recusandae molestiae maiores deserunt numquam sunt commodi eveniet ipsa unde!
+        <span class="text-[40px] pop font-[500] 1200:text-[28px]"
+          >Savety camping</span
+        >
+        <p
+          class="font-[poppins] 1500:text-[19px] 1200:text-[18px] txt 1500:mt-[10px] font-[500] text-[gray] text-[22px] mt-[18px]"
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia
+          molestias recusandae molestiae maiores deserunt numquam sunt commodi
+          eveniet ipsa unde!
         </p>
       </div>
     </div>
@@ -62,22 +154,24 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped>
-
+@media screen and (min-width:  1200px) {
+  .x{
+    width: 45%;
+  }
+}
 @media screen and (max-width: 666px) {
-  .heading{
-  font-size: 30px;
+  .heading {
+    font-size: 30px;
+  }
+  .txt {
+    font-size: 13px;
+  }
 }
-.txt{
-  font-size: 13px;
-}
-}
-.pop{
-  font-family: 'Poppins', sans-serif;
+.pop {
+  font-family: "Poppins", sans-serif;
 }
 </style>
