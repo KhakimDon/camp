110ca0de77d1080f4f1cd3f954d554769177a856
khakimdon <almamedovkh@mail.ru>
2023-06-09 07:03:57 +0500

    nine

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -21,7 +21,7 @@
   <body class="h-full">
   ```
 -->
-  <div class="w-[666px] 666:w-[100%]  mx-auto">
+  <div class="w-[666px] 666:w-[100%] mx-auto">
     <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
@@ -37,7 +37,7 @@
       </div>
 
       <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="telegram.php" method="POST">
+        <form @submit.prevent="send()" class="space-y-6" method="POST">
           <div>
             <label
               for="name"
@@ -46,6 +46,7 @@
             >
             <div class="mt-2">
               <input
+                v-model="username"
                 id="name"
                 name="user_name"
                 type="text"
@@ -67,9 +68,9 @@
             </div>
             <div class="mt-2">
               <input
-                @input="input"
-                value="+998"
+                v-model="usernumber"
                 id="password"
+                @input="input()"
                 placeholder="+998 91 549-17-54"
                 name="user_phone"
                 type="text"
@@ -93,22 +94,59 @@
         <p class="mt-10 text-center text-sm text-gray-500">
           Администрация свяжется свами в течении дня.
         </p>
-
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
 export default {
+ 
+  data() {
+    return {
+      username: "",
+      usernumber: "",
 
+      // telegram bot config
+      token: "6104797248:AAEti3PIansCHmv1kPY_HSki4Q8wq2PJa5c",
+      chatId: 768959234,
+    };
+  },
   methods: {
-    input(){
-        let char =event.target.value[event.target.value.length - 1].charCodeAt()
-        if(char >= 48 &&  char <= 57 || char == 43 || char == 32 || char == 45){}
-        else event.target.value = event.target.value.substr(0, event.target.value.length -1)
-    }
+    send() {
+      let fullmessage = `Имя клиента: ${this.username} \n
+      
+      Телефон номер: ${this.usernumber}`;
+      console.log(fullmessage);
+      // console.log(this.$http);
+      // let a = `https://api.telegram.org/bot${this.token}:AAEti3PIansCHmv1kPY_HSki4Q8wq2PJa5c/sendMessage?chat_id=${this.chatId}&text=${fullmessage}`;
+
+      let api = `https://api.telegram.org/bot6104797248:AAEti3PIansCHmv1kPY_HSki4Q8wq2PJa5c/sendMessage?chat_id=-821000015&text=${fullmessage}`
+
+
+      this.$http.post(api).then(
+        (response) => {
+          console.log("Succsessfully!", response.data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    },
+    input() {
+      let char = event.target.value[event.target.value.length - 1].charCodeAt();
+      if (
+        (char >= 48 && char <= 57) ||
+        char == 43 ||
+        char == 32 ||
+        char == 45
+      ) {
+      } else
+        event.target.value = event.target.value.substr(
+          0,
+          event.target.value.length - 1
+        );
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+// import VueResource from 'vue-resource'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 
 
 const app = createApp(App)
 
+app.use(VueAxios, axios)
 app.use(router)
+// app.use(VueResource)
 
 
 app.mount('#app')
